dacb44681611e15c04aa8055c9f7b602cf51959e
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-09-05 01:47:28 -0700

    Add renderEmployeesByDepartment function

--- server.js
+++ server.js
@@ -152,3 +152,22 @@ const renderAllEmployees = () => {
         questions();
     });
 };
+
+const renderEmployeesByDepartment = () => {
+    const sql = `SELECT employee.first_name,
+                employee.last_name,
+                department.name AS department
+                FROM employee
+                LEFT JOIN role ON employee.role_id = role.id
+                LEFT JOIN department ON role.department_id = department.id`;
+
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        console.log(chalk.green.italic(`====================================================================================`));
+        console.log(`                              ` + chalk.green.bold(`Employees by Department:`));
+        console.log(chalk.green.italic(`====================================================================================`));
+        console.table(res);
+        console.log(chalk.green.italic(`====================================================================================`));
+        questions();
+    });
+};
